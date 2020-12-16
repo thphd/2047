ba52ea151e4950ee7436e107cfbdc30a28128f23
Tsai1993 <caiw15@mails.tsinghua.edu.cn>
2020-12-16 15:22:48 +0800

    refactor

--- main.py
+++ main.py
@@ -32,48 +32,17 @@ wait_for_database_online()
 
 from medals import get_medals, get_user_medals
 
-from flask_cors import CORS
-
 import flask
 from flask import Flask, g, abort # session
-
-# FUCK YOU, FLASK
-# https://github.com/pallets/flask/issues/2989#issuecomment-695412677
-class FlaskPatched(Flask):
-    def select_jinja_autoescape(self, filename):
-        """Returns ``True`` if autoescaping should be active for the given
-        template name. If no template name is given, returns `True`.
-
-        .. versionadded:: 0.5
-        """
-        if filename is None:
-            return True
-        return filename.endswith(('.html', '.htm', '.xml', '.xhtml', 'html.jinja'))
-
 from flask import render_template, request, send_from_directory, make_response
 # from flask_gzip import Gzip
-
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 from api import api_registry, get_categories_info, get_url_to_post, get_url_to_post_given_details
 from api import *
 
-# from quotes import get_quote
-
 from session import save_session,load_session
 
-# init_directory('./static/')
-# init_directory('./static/upload/')
-
-app = FlaskPatched(__name__, static_url_path='')
-
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
-
-# app.secret_key = get_secret()
-CORS(app)
-from flask_response_gzip import gzipify
-gzipify(app)
+from app import app
 
 def route(r):
     def rr(f):
