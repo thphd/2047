8ffadfd889f812948071a902a158a7537759da7a
Tsai1993 <caiw15@mails.tsinghua.edu.cn>
2020-12-20 05:03:11 +0800

    feedback for content 2 long

--- api.py
+++ api.py
@@ -392,9 +392,10 @@ def _():
     return {'logout':True}
 
 def content_length_check(content, allow_short=False):
-    if len(content)>20000:
-        raise Exception('content too long')
-    if len(content)<2 and allow_short==False:
+    maxlen = 40000
+    if len(content)>maxlen:
+        raise Exception('content too long {}/{}'.format(len(content), maxlen))
+    if (len(content)<2 and allow_short==False) or len(content)==0:
         raise Exception('content too short')
 
 def title_length_check(title):
